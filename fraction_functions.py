7406a26ba5744695b1082ab33310c3e5b0da1fe9
hananja <e.loos@schule.bremen.de>
2020-01-22 11:26:03 +0100

    reformat

--- fraction_functions.py
+++ fraction_functions.py
@@ -1,18 +1,20 @@
 # Funktionen fuer Brueche in Tupeln (Uebungsaufgabe)
 
 def printFract(fraction):
-   print(fraction[0]) # Zaehler ausgeben
-   print("---------")
-   print(fraction[1]) # Nenner ausgeben
+    print(fraction[0])  # Zaehler ausgeben
+    print("---------")
+    print(fraction[1])  # Nenner ausgeben
+
 
 def negFract(fraction):
-   result = (-fraction[0], fraction[1])
-   return result
+    result = (-fraction[0], fraction[1])
+    return result
+
 
 def inputFract():
-   numerator = input("Bitte Zaehler eingeben: ")
-   denominator = input("Bitte Nenner eingeben: ")
-   return (int(numerator), int(denominator))
+    numerator = input("Bitte Zaehler eingeben: ")
+    denominator = input("Bitte Nenner eingeben: ")
+    return (int(numerator), int(denominator))
 
 
 # Hauptprogramm
