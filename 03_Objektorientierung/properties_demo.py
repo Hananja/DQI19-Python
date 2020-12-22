3dd6174f7a4ecf65ba96b932a67d10fc40088848
E. Loos <Hananja@github.com>
2020-12-22 14:09:56 +0100

    new Files

---
+++ 03_Objektorientierung/properties_demo.py
@@ -0,0 +1,48 @@
+# Demonstration von Properties
+
+class MyClassVanilla:
+    """ Klasse mit einfachem Attribut (ohne Propertie) """
+
+    def __init__(self):
+        self.value = 0
+
+
+class MyClassProperties:
+    """ Klasse mit Propertie, das im Setter überprüft wird.
+        Entscheidendes Kriterium: Eine nachträgliche Abstraktion
+        durch Datenkapselung (Geheimnisprinzip) ist ohne
+        Veränderung der Schnittstelle möglich.
+    """
+
+    def __init__(self):
+        self.value = 0
+
+    @property
+    def value(self):
+        return self.__value  # stark privat durch __ am Anfang
+        # (vgl. https://www.python.org/dev/peps/pep-0008/#descriptive-naming-styles)
+
+    @value.setter
+    def value(self, value):
+        if value < 0:
+            raise ValueError("value parameter must not be <0")
+        else:
+            self.__value = value
+
+
+oop = MyClassVanilla()
+print(oop.value)
+oop.value = 1
+print(oop.value)
+oop.value = -1
+print(oop.value)
+
+try:
+    omp = MyClassProperties()
+    print(omp.value)
+    omp.value = 1
+    print(omp.value)
+    omp.value = -1  # Bang!
+    print(omp.value)
+except ValueError as e:
+    print(f"Error: {e}")
